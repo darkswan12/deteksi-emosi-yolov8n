d26785d16865593634df09deb78cf0a17277c9ff
darmawan suhara <dsuharaaa@gmail.com>
2025-08-29 20:20:40 +0700

    edit app.py lagi dan lagi dan lagi

--- app.py
+++ app.py
@@ -67,8 +67,21 @@ camera_option = st.selectbox(
 
 # --- Konfigurasi WebRTC
 RTC_CONFIGURATION = RTCConfiguration(
-    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+    {
+        "iceServers": [
+            {"urls": ["stun:stun.l.google.com:19302"]},
+            {"urls": ["stun:stun1.l.google.com:19302"]},
+            {"urls": ["stun:stun2.l.google.com:19302"]},
+            {"urls": ["stun:stun3.l.google.com:19302"]},
+            {"urls": ["stun:stun4.l.google.com:19302"]},
+            {"urls": ["stun:stun.services.mozilla.com"]},
+            {"urls": ["stun:stun.nextcloud.com:3478"]},
+            {"urls": ["stun:stun.stunprotocol.org:3478"]},
+            {"urls": ["stun:stun.voipbuster.com:3478"]},
+        ]
+    }
 )
+
 
 # === Video Processor ===
 class EmotionProcessor(VideoProcessorBase):
